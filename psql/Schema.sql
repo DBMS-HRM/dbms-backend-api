d538b88d2712497f31f0a1a92ce49ba957412db9
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-01-07 11:34:44 +0530

    start working on schema

--- psql/Schema.sql
+++ psql/Schema.sql
@@ -1,20 +1,36 @@
-DROP TABLE IF EXISTS doc;
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-   name VARCHAR(20) PRIMARY KEY,
-   age INTEGER
+CREATE TABLE branch (
+    branch_ID uuid PRIMARY KEY,
+    branch_name VARCHAR(20)
 );
 
-CREATE TABLE doc (
-   name VARCHAR(20) PRIMARY KEY,
-   age INTEGER
-);
+CREATE TABLE employee_company_details (
+    employee_id uuid PRIMARY KEY,
+    branch_id uuid ,
+    job_title VARCHAR(20),
+    employment_status VARCHAR(20),
+    pay_grade VARCHAR(20),
+    department_name VARCHAR(20),
+    FOREIGN KEY(branch_id) REFERENCES branch(branch_id),
+    FOREIGN KEY(job_title) REFERENCES job_title(job_title),
+    FOREIGN KEY(employment_status) REFERENCES job_title(employment_status),
+    
+); 
 
-INSERT INTO users(name, age) VALUES ('Pasindu1', 22);
-INSERT INTO users(name, age) VALUES ('Pasindu2', 20);
-INSERT INTO users(name, age) VALUES ('Pasindu3', 18);
-INSERT INTO users(name, age) VALUES ('Yasith', 23);
+CREATE TABLE user (
+    user_id uuid PRIMARY KEY,
+    employee_id VARCHAR(50),
+)
+
+CREATE TABLE employee_user (
+    user_id uuid PRIMARY KEY,
+    employee_id uuid,
+    FOREIGN KEY ()
+)
 
 
-SELECT name, age FROM users;
+
+CREATE TABLE employee_personal_detail (
+    employee_id uuid PRIMARY KEY,
+    name VARCHAR(20),
+    date_of_birth DATE
+)
