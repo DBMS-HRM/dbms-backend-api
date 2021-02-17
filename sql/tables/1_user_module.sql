f5a3390c0c07628d242ebcdfe006b83652fe28c3
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-02-17 21:01:41 +0530

    add er diagram

--- sql/tables/1_user_module.sql
+++ sql/tables/1_user_module.sql
@@ -37,14 +37,6 @@ CREATE TABLE  employment_status (
     employment_status VARCHAR(20) PRIMARY KEY
 );
 
-CREATE TABLE leave_request_state (
-    leave_state VARCHAR(20) PRIMARY KEY
-);
-
-CREATE TABLE leave_type (
-    leave_type VARCHAR(20) PRIMARY KEY
-);
-
 -- ---------------------------------------------------------------------------------------------------------------------
 
 CREATE TABLE branch (
@@ -129,55 +121,3 @@ CREATE TABLE admin_account (
     user_id UUID PRIMARY KEY,
     FOREIGN KEY (user_id) REFERENCES user_account(user_id) ON DELETE RESTRICT
 );
-
-
--- Leave Tables --------------------------------------------------------------------------------------------------------
-
-CREATE TABLE leave_request (
-    leave_id UUID PRIMARY KEY,
-    employee_id UUID NOT NULL,
-    request_date DATE NOT NULL,
-    leave_state VARCHAR(20) NOT NULL,
-    leave_type VARCHAR(20) NOT NULL,
-    approved_date DATE NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES employee_company_detail(employee_id) ON DELETE RESTRICT,
-    FOREIGN KEY (leave_state) REFERENCES leave_request_state(leave_state) ON DELETE RESTRICT,
-    FOREIGN KEY (leave_type) REFERENCES leave_type(leave_type) ON DELETE RESTRICT
-);
-
-CREATE TABLE leave_approval_supervisor (
-    leave_id UUID PRIMARY KEY,
-    supervisor_id UUID NOT NULL,
-    FOREIGN KEY (leave_id) REFERENCES leave_request(leave_id) ON DELETE RESTRICT,
-    FOREIGN KEY (supervisor_id) REFERENCES employee_company_detail(employee_id) ON DELETE RESTRICT
-);
-
-
-
--- ██╗░░░██╗██╗███████╗░██╗░░░░░░░██╗░██████╗
--- ██║░░░██║██║██╔════╝░██║░░██╗░░██║██╔════╝
--- ╚██╗░██╔╝██║█████╗░░░╚██╗████╗██╔╝╚█████╗░
--- ░╚████╔╝░██║██╔══╝░░░░████╔═████║░░╚═══██╗
--- ░░╚██╔╝░░██║███████╗░░╚██╔╝░╚██╔╝░██████╔╝
--- ░░░╚═╝░░░╚═╝╚══════╝░░░╚═╝░░░╚═╝░░╚═════╝░
-
-
-
-
--- ███████╗██╗░░░██╗███╗░░██╗░█████╗░████████╗██╗░█████╗░███╗░░██╗░██████╗
--- ██╔════╝██║░░░██║████╗░██║██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║██╔════╝
--- █████╗░░██║░░░██║██╔██╗██║██║░░╚═╝░░░██║░░░██║██║░░██║██╔██╗██║╚█████╗░
--- ██╔══╝░░██║░░░██║██║╚████║██║░░██╗░░░██║░░░██║██║░░██║██║╚████║░╚═══██╗
--- ██║░░░░░╚██████╔╝██║░╚███║╚█████╔╝░░░██║░░░██║╚█████╔╝██║░╚███║██████╔╝
--- ╚═╝░░░░░░╚═════╝░╚═╝░░╚══╝░╚════╝░░░░╚═╝░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░
-
-
-
-
-
--- ██████╗░██████╗░░█████╗░░█████╗░███████╗██████╗░██╗░░░██╗██████╗░███████╗░██████╗
--- ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██║░░░██║██╔══██╗██╔════╝██╔════╝
--- ██████╔╝██████╔╝██║░░██║██║░░╚═╝█████╗░░██║░░██║██║░░░██║██████╔╝█████╗░░╚█████╗░
--- ██╔═══╝░██╔══██╗██║░░██║██║░░██╗██╔══╝░░██║░░██║██║░░░██║██╔══██╗██╔══╝░░░╚═══██╗
--- ██║░░░░░██║░░██║╚█████╔╝╚█████╔╝███████╗██████╔╝╚██████╔╝██║░░██║███████╗██████╔╝
--- ╚═╝░░░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚══════╝╚═════╝░░╚═════╝░╚═╝░░╚═╝╚══════╝╚═════╝░
---
+++ sql/tables/2_report_module.sql
@@ -0,0 +1,31 @@
+
+CREATE TABLE leave_request_state (
+    leave_state VARCHAR(20) PRIMARY KEY
+);
+
+
+CREATE TABLE leave_type (
+    leave_type VARCHAR(20) PRIMARY KEY
+);
+
+
+-- Leave Tables --------------------------------------------------------------------------------------------------------
+
+CREATE TABLE leave_request (
+    leave_id UUID PRIMARY KEY,
+    employee_id UUID NOT NULL,
+    request_date DATE NOT NULL,
+    leave_state VARCHAR(20) NOT NULL,
+    leave_type VARCHAR(20) NOT NULL,
+    approved_date DATE NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employee_company_detail(employee_id) ON DELETE RESTRICT,
+    FOREIGN KEY (leave_state) REFERENCES leave_request_state(leave_state) ON DELETE RESTRICT,
+    FOREIGN KEY (leave_type) REFERENCES leave_type(leave_type) ON DELETE RESTRICT
+);
+
+CREATE TABLE leave_approval_supervisor (
+    leave_id UUID PRIMARY KEY,
+    supervisor_id UUID NOT NULL,
+    FOREIGN KEY (leave_id) REFERENCES leave_request(leave_id) ON DELETE RESTRICT,
+    FOREIGN KEY (supervisor_id) REFERENCES employee_company_detail(employee_id) ON DELETE RESTRICT
+);
