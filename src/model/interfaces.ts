b467d7e77d813cd47df897e66d35cf980543f139
Pasindu <pasindua.18@cse.mrt.ac.lk>
2021-02-22 01:23:08 +0530

    Few changes in user model

---
+++ src/model/interfaces.ts
@@ -0,0 +1,81 @@
+/**
+ * Admin Account interface
+ */
+
+export interface AdminAccount {
+    userId : string;
+    username : string;
+    password : string;
+    email : string;
+    status : boolean;
+    branchId : string;
+    accountType : string;
+}
+
+/**
+ * Employee Account interfaces
+ */
+export interface EmployeeAccount {
+    userId : string;
+    username : string;
+    password : string;
+    email : string;
+    accountType : string;
+}
+
+export interface EmployeeCompanyDetail {
+    employeeId : string;
+    branchId : string;
+    jobTitle : string;
+    employmentStatus : string;
+    payGrade : string;
+    departmentName : string
+}
+
+export interface EmployeeEmergencyDetail {
+    employeeId : string;
+    phoneNumber : string;
+    address : string;
+    emailAddress : string;
+}
+
+export interface EmployeePersonalDetail {
+    employeeId : string;
+    name : string;
+    dateOfBirth : typeof Date;
+    maritalStatus : boolean;
+}
+
+/**
+ * Employee related Types
+ */
+export interface Branch {
+    branchId : string;
+    branchName : string;
+}
+
+export interface Department{
+    departmentName : string
+}
+
+export interface JobTitle {
+    jobTitle : string
+}
+
+export interface EmploymentStatus {
+    employmentStatus : string;
+}
+
+export interface PayGrade {
+    payGrade : string;
+    numOfLeaves : number,
+}
+
+/**
+ * Leave related Types
+ */
+
+
+/**
+ * Report related Types
+ */
